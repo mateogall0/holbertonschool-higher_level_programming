77ad390f728321adc49be183bf3e94296adf1333
gallit0 <mateogesede@gmail.com>
2022-09-05 19:07:13 -0700

    task 7 edge case fixed

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
+    if c == '':
+        print("Traceback (most recent call last):")
+        exit() 
     if c >= 'a' and c <= 'z':
         return True
     return False
