9a76d60fda09cd9ad48ab7668c78cac173ee5d4c
gallit0 <mateogesede@gmail.com>
2022-09-05 20:06:58 -0700

    task 12 fixed

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if i % 15 == 0:
-        print('FizzBuzz', end=" ")
-    elif i % 5 == 0:
-        print("Buzz", end=" ")
-    elif i % 3 == 0:
-        print("Fizz", end=" ")
-    else:
-        print("{:n}".format(i), end=' ')
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print('FizzBuzz', end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        else:
+            print("{:n}".format(i), end=' ')
