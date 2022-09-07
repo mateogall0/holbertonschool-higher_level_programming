b067bc46ab1af6013e2eb9335489dff47f3b61cd
gallit0 <mateogesede@gmail.com>
2022-09-07 08:57:05 -0700

    task 3 finished

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 def atoi(string):
     res = 0
-    for i in range(len(string)):
-        res = res * 10 + (ord(string[i]) - ord('0'))
-    return res
+    sign = 1
+    i = 0
+
+    if string[0] == '-':
+        sign = -1
+        i += 1
+
+    for j in range(i, len(string)):
+        res = res * 10 + (ord(string[j]) - ord('0'))
+    return res * sign
 
 
 if __name__ == '__main__':
