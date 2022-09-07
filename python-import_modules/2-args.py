dc34608bb4f55a244fbb820dd2fabce3b5196feb
gallit0 <mateogesede@gmail.com>
2022-09-07 07:36:18 -0700

    task 2 finished

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    argc = len(sys.argv) - 1
+    argc = len(sys.argv)
     j = 1
-    if argc == 0:
+    if argc == 1:
         print('0 arguments.')
         exit()
     print('{} arguments:'.format(argc))
