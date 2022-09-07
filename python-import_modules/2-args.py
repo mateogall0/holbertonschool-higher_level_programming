db7c9eb29a9f4142e9e44c648f720dbbe1c35537
gallit0 <mateogesede@gmail.com>
2022-09-07 07:37:21 -0700

    task 2 re fixed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     if argc == 1:
         print('0 arguments.')
         exit()
-    print('{} arguments:'.format(argc))
+    print('{} arguments:'.format(argc - 1))
     for i in range(1, argc):
         print('{:n}: {:s}'.format(j, sys.argv[i]))
         j += 1
