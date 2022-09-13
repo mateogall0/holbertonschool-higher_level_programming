63c4c8643ab364fde3b219e11e73157d5177ee95
gallit0 <mateogesede@gmail.com>
2022-09-13 17:11:41 -0300

    task 2 finished

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             res += 1
-        except TypeError:
+        except ValueError:
             None
     print()
     return res
