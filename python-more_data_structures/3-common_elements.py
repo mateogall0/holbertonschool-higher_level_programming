735f5211bcd9f16b8a9e4f76768665bef82334c9
gallit0 <mateogesede@gmail.com>
2022-09-12 22:42:24 -0300

    task 3 done

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for j in set_2:
+    l1 = list(set_1)
+    l2 = list(set_2)
+    new = []
+
+    for i in l1:
+        for j in l2:
             if i == j:
-                return i
+                new.append(l1[i])
+    return dict.fromkeys(new)
