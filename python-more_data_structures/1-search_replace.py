15b0d22c8d0dff9cac4f9ab69b2f5193fb31e117
gallit0 <mateogesede@gmail.com>
2022-09-12 21:58:13 -0300

    task 1 fixed edge cases

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list or not search or not replace:
-        return my_list
     new = []
     for i in range(len(my_list)):
         if my_list[i] == search:
