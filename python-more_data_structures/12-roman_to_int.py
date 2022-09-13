d9243186187dc5276fe60ca6ecc38f0909f24ba4
gallit0 <mateogesede@gmail.com>
2022-09-13 15:22:15 -0300

    task 12 edge cases fixed

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
-        return None
+        return 0
     result = 0
     for i in roman_string:
         if i == 'I':
