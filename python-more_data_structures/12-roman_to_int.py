df083fb9ea1000c939a1858d91ca38633a7ed371
gallit0 <mateogesede@gmail.com>
2022-09-13 15:37:32 -0300

    task 12 done

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,20 +2,11 @@
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
-    result = 0
-    for i in roman_string:
-        if i == 'I':
-            result += 1
-        if i == 'V':
-            result += 5
-        if i == 'X':
-            result += 10
-        if i == 'L':
-            result += 50
-        if i == 'C':
-            result += 100
-        if i == 'D':
-            result += 500
-        if i == 'M':
-            result += 1000
-    return result
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
+            res += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
+        else:
+            res += roman[roman_string[i]]
+    return res
