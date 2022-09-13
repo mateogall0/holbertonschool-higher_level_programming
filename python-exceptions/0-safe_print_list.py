46381687b4d083e4a1fe8eadfd4ace43e7becef4
gallit0 <mateogesede@gmail.com>
2022-09-13 16:37:06 -0300

    edge cases fixed

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,6 +4,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
         except:
-            break
+            print()
+            return i
     print()
-    return i
+    return x
