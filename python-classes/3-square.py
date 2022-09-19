90f4a12da1403e4ba75c02f893e219857c630a69
gallit0 <mateogesede@gmail.com>
2022-09-19 17:39:10 -0300

    task 3 done

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+class Square():
+    """Square"""
+    __size = None
+
+    def __init__(self, size=0):
+        try:
+            if size % 1 != 0:
+                raise TypeError()
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        except TypeError:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size ** 2
