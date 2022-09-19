1c5fd6fb2711a3278e2737da196352a9cba5552a
gallit0 <mateogesede@gmail.com>
2022-09-19 17:33:47 -0300

    task 2 finished

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+class Square():
+    """Square"""
+    __size = None
+
+    def __init__(self, size=0):
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        except TypeError:
+            raise TypeError("size must be an integer")
