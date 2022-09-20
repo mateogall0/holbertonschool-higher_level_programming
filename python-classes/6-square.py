20506a0a5163e1b973696cfc38c775e440e49a77
gallit0 <mateogesede@gmail.com>
2022-09-19 22:03:20 -0300

    task 6 done

---
+++ python-classes/6-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+class Square():
+    """Square"""
+    __size = 0
+    __position = (0, 0)
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__position[1]):
+            print()
+        for _ in range(self.__size):
+            for _ in range(self.__position[0]):
+                print(end=' ')
+            for _ in range(self.__size):
+                print('#', end='')
+            print()
