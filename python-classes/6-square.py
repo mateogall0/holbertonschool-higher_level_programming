9b7053b18c1dc935c4a9ba70ef81e3de4897ef3e
gallit0 <mateogesede@gmail.com>
2022-09-19 22:10:13 -0300

    task 6 done

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -2,12 +2,23 @@
 """Square"""
 
 
+from turtle import pos
+
+
 class Square():
     """Square"""
     __size = 0
     __position = (0, 0)
 
     def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if not isinstance(position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
         self.__position = position
 
@@ -17,6 +28,10 @@ class Square():
     @property
     def size(self):
         return self.__size
+
+    @property
+    def position(self):
+        return self.__position
 
     @size.setter
     def size(self, value):
