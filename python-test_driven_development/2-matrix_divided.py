444d3d138dbd6f1285f553a4dc1e2247ba148532
Mateo Gallo <5073@holbertonstudents.com>
2022-09-22 15:35:33 -0300

    task 2 done

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,6 +4,9 @@
 
 def matrix_divided(matrix, div):
     """matrix resurrections"""
+    te = 'matrix must be a matrix (list of lists) of integers/floats'
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError('div must be a number')
     if div == 0:
         raise ZeroDivisionError('division by zero')
     l = len(matrix[0])
@@ -14,7 +17,6 @@ def matrix_divided(matrix, div):
         m.append([])
         for j in matrix[i]:
             if not isinstance(j, float) and not isinstance(j, int):
-                te = 'matrix must be a matrix (list of lists) of integers/floats'
                 raise TypeError(te)
             m[i].append(round(j / div, 2))
     return m
