63407b941e9415ba618fd370e763a7944a45f5f4
Mateo Gallo <5073@holbertonstudents.com>
2022-09-22 18:51:19 -0300

    task 5 done

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,14 +7,13 @@ def text_indentation(text):
     if type(text) != str:
         raise TypeError('text must be a string')
     t = text
-    s = ''
-
     i = 0
     while i < len(t):
-        s += t[i]
-        if t[i] == ':' or t[i] == '?' or t[i] == '.':
-            s += '\n\n'
-            while i < len(t) and t[i] == ' ':
+        print(t[i], end='')
+        if t[i] in ':?.' or t[i] == '\n':
+            print(end='\n\n')
+            i += 1
+            while i < len(t) and (t[i] == ' ' or t[i] == '\t'):
                 i += 1
+            continue
         i += 1
-    print(s)
