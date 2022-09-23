a65f97412e3524296d202fd963045d1a758b84b2
Mateo Gallo <5073@holbertonstudents.com>
2022-09-22 23:25:40 -0300

    edge cases fixed at task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,9 +6,16 @@ class Rectangle:
     """Rectangle"""
 
     def __init__(self, width=0, height=0):
+        if type(height) != int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        if type(width) != int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
         self.__height = height
-        self.__width = width
-        
+        self.__width = width  
 
     @property
     def width(self):
