0a8310eafdf1cc3d3dcde39d361ef68e5ddad6b8
gallit0 <mateogesede@gmail.com>
2022-09-27 17:16:05 -0300

    task 12 done

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -5,7 +5,13 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-    l = []
+    lis = []
     for i in range(n):
-        l.append([1] * (i + 1))
-    return l
+        lis.append([1] * (i + 1))
+    try:
+        for i in range(2, len(l)):
+            for j in range(1, len(l[i]) - 1):
+                lis[i][j] = lis[i - 1][j - 1] + lis[i - 1][j]
+    except Exception:
+        pass
+    return lis
