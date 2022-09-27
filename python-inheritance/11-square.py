4db0b8803dce190277487a25dee8f7406ebab69c
gallit0 <mateogesede@gmail.com>
2022-09-27 00:45:54 -0300

    task 11 is task 10

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square"""
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
