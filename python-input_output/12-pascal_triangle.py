10e890eac5582b6f05acdbf428cfce15a47e2e3a
gallit0 <mateogesede@gmail.com>
2022-09-27 17:17:59 -0300

    task 12 fixed

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -10,8 +10,8 @@ def pascal_triangle(n):
     for i in range(n):
         lis.append([1] * (i + 1))
     try:
-        for i in range(2, len(l)):
-            for j in range(1, len(l[i]) - 1):
+        for i in range(2, len(lis)):
+            for j in range(1, len(lis[i]) - 1):
                 lis[i][j] = lis[i - 1][j - 1] + lis[i - 1][j]
     except Exception:
         pass
