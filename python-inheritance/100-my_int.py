b94c206081d9fd8c81416b8dc8d52e302b648ffb
gallit0 <mateogesede@gmail.com>
2022-09-27 23:20:10 -0300

    task 12 done

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class Int:
+    """Class INT"""
+    pass
+
+
+class MyInt(Int):
+    """MyINT"""
+    def __init__(self, num):
+        self.__num = num
+
+    def __eq__(self, other):
+        if self.__num == other:
+            return False
+        return True
+
+    def __ne__(self, other):
+        if self.__num != other:
+            return False
+        return True
