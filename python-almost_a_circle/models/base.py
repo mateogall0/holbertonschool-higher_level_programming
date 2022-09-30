cbc1d05f0556b7a8f2c953af0019f0bbedf4fcae
gallit0 <mateogesede@gmail.com>
2022-09-30 00:36:31 -0300

    task 0 done

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id == None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
