10789f474ed903ca54a4f86c15f198dfff7ba5da
gallit0 <mateogesede@gmail.com>
2022-09-30 01:26:25 -0300

    task 7 done

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -83,7 +83,9 @@ class Rectangle(Base):
 
     def display(self):
         """Prints the rectangle in stdout"""
+        print('\n' * self.y, end='')
         for _ in range(self.height):
+            print(' ' * self.x, end='')
             print('#' * self.width)
 
     def __str__(self):
