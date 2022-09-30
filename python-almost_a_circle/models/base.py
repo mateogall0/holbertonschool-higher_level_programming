9005c965e56e285018cd1ef2083e4da837fcc35d
gallit0 <mateogesede@gmail.com>
2022-09-30 02:48:14 -0300

    task 15 fixed

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,6 +20,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """Class to json_string"""
         if list_dictionaries is None:
-            return []
+            return '[]'
 
         return json.dumps(list_dictionaries)
