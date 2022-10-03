b233de585dd52a42407943b7f04e1958789cebed
gallit0 <mateogesede@gmail.com>
2022-10-03 10:44:21 -0300

    WIP: attempt at checker tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -15,6 +15,21 @@ class testsNumber0(unittest.TestCase):
     def test_0(self):
         self.assertEqual(self.base1.id, 1)
 
+    def test_1(self):
+        self.assertEqual(self.base1.id, 2)
+
+    def test_2(self):
+        self.assertEqual(self.base1.id, 3)
+
+    def test_3(self):
+        self.assertEqual(self.base1.id, 4)
+
+    def test_4(self):
+        self.assertEqual(self.base1.id, 5)
+
+    def test_5(self):
+        self.assertEqual(self.base1.id, 6)
+
 
 class testsNumber1(unittest.TestCase):
     """Tests number 1"""
