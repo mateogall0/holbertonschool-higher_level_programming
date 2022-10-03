8f7520447d0c68a4661ef8b4a9539f847ec69507
gallit0 <mateogesede@gmail.com>
2022-10-03 13:17:47 -0300

    WIP: tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -14,6 +14,16 @@ class testsNumber0(unittest.TestCase):
         self.assertEqual(self.r1.id, 13)
         self.assertEqual(self.r1.width, 1)
         self.assertEqual(self.r1.height, 2)
-
+        self.r2 = Rectangle(1, 2, 3, 4, 5)
+    def test_1(self):
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+        with self.assertRaises(TypeError):
+            Rectangle(1, "2")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, "3")
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "4")
+        
 if __name__ == '__main__':
     unittest.main()
