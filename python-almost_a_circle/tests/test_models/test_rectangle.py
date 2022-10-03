d4951c81cdc9060c7d63ecf87ed0a74302794917
gallit0 <mateogesede@gmail.com>
2022-10-03 13:36:21 -0300

    WIP: tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,11 +11,11 @@ class testsNumber0(unittest.TestCase):
     automatically an ID exists"""
     def test_0(self):
         self.r1 = Rectangle(1, 2)
-        self.assertEqual(self.r1.id, 13)
+        self.assertEqual(self.r1.id, 14)
         self.assertEqual(self.r1.width, 1)
         self.assertEqual(self.r1.height, 2)
         self.r2 = Rectangle(1, 2, 3, 4, 5)
-        self.assertEqual(self.r2.height, 2)
+        self.assertEqual(self.r2.id, 5)
     def test_1(self):
         with self.assertRaises(TypeError):
             Rectangle("1", 2)
@@ -32,11 +32,16 @@ class testsNumber0(unittest.TestCase):
         with self.assertRaises(ValueError):
             Rectangle(0, 2)
         with self.assertRaises(ValueError):
+            Rectangle(1, 0)
+        with self.assertRaises(ValueError):
             Rectangle(1, 2, -3)
         with self.assertRaises(ValueError):
             Rectangle(1, 2, 3, -4)
         
-        
+class TestsNumber1(unittest.TestCase):
+    def test_0(self):
+         self.r3 = Rectangle(3, 3)
+         self.assertEqual(self.r3.area(), 9)
         
 if __name__ == '__main__':
     unittest.main()
