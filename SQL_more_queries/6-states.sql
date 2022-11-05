e598d688aa54f5500ccdd4cd46680455827d0f51
gallit0 <mateogesede@gmail.com>
2022-11-05 17:24:21 -0300

    task 6 done

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- create database
+CREATE DATABASE hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT UNIQUE AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id)
+)
