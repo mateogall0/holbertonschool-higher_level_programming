4013a6c2e55d4a05fd62e582e0f02729969fc165
gallit0 <mateogesede@gmail.com>
2022-11-05 17:52:24 -0300

    task 9 done

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- list all cities
-SELECT cities.id, cities.name, states.name FROM cities, states
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id
