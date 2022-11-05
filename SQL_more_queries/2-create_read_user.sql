e988a9220dce5d2059b3c60a36b558b81e33bb7b
gallit0 <mateogesede@gmail.com>
2022-11-05 14:25:11 -0300

    task 2 done

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- create a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
+GRANT SELECT PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
