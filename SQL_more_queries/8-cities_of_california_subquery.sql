b4f6cac462e9aa1fe218fe93825842d6f25ad15a
gallit0 <mateogesede@gmail.com>
2022-11-05 17:35:55 -0300

    task 8 done

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,5 @@
 -- list all cities of california
-SELECT id AS ca FROM states WHERE name="California";
-SELECT * FROM cities WHERE state_id=ca
+--SELECT id AS ca FROM states WHERE name="California";
+--SELECT * FROM cities WHERE state_id=ca
+
+SELECT * FROM cities WHERE state_id=(id FROM states WHERE name="California")
