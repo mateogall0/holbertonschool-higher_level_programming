46b745038cb1e798ab08d75455300ac8e58e7102
gallit0 <mateogesede@gmail.com>
2022-11-06 11:35:59 -0800

    task 12 done

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- list shows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = NULL 
+JOIN tv_show_genres ON tv_shows.id = NULL 
 ORDER BY tv_shows.title, tv_show_genres.genre_id
