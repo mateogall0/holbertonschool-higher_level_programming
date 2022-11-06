195e6650350ba6f8433160657be61a540448a672
gallit0 <mateogesede@gmail.com>
2022-11-06 15:28:27 -0800

    task 13 done

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,5 @@
 -- list shows
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT name AS genre, COUNT(*) AS number_of_shows
 FROM tv_shows
-COUNT (*)
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+ORDER BY number_of_shows DESC
