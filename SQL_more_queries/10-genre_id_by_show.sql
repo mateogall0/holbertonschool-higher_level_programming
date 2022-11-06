e59afe3171d68618d171fe57fdb7522cd4dbec00
gallit0 <mateogesede@gmail.com>
2022-11-06 11:08:10 -0800

    task 10 done

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- databse dump
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
