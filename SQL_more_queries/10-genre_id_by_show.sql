df1b06837bfd4126d295e9d0a0defc60892fec19
gallit0 <mateogesede@gmail.com>
2022-11-06 11:17:58 -0800

    task 10 done

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
 GROUP BY tv_shows.title, tv_show_genres.genre_id
-ORDER BY tv_show_genres.genre_id ASC
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
