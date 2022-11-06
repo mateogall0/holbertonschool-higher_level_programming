9d87dcacc8747d36ba87ad81a148e1d7565888dd
gallit0 <mateogesede@gmail.com>
2022-11-06 15:37:50 -0800

    task 13 done

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- list shows
-SELECT tv_shows.name AS genre, COUNT(*) AS number_of_shows
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.name
 ORDER BY number_of_shows DESC
