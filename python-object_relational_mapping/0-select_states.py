737c13c5e29101f9e6a16434149228017b8e1611
gallit0 <mateogesede@gmail.com>
2022-11-08 12:07:39 -0800

    task 0 done

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""list all states"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        exit()
+    db = MySQLdb.connect(host = "localhost",
+        port = 3306,
+        user = sys.argv[1],
+        passwd = sys.argv[2],
+        db = sys.argv[3]
+    )
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY states.id")
+    m = c.fetchone()
+    print(m)
+    db.close()
