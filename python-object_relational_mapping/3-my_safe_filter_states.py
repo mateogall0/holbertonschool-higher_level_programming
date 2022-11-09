5e0395c15b05345863485473b02c565bba6d357d
gallit0 <mateogesede@gmail.com>
2022-11-09 15:34:32 -0800

    task 3 attempt at checker

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""list all states"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        exit()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    c = db.cursor()
+    s = 'SELECT * FROM states WHERE name LIKE "{}"'.format(sys.argv[4])
+    c.execute(s)
+    rows = c.fetchall()
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    db.close()
