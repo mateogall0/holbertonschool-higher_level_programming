d4d85f8ba7feb9792afcd2286ec5346dc2b88c39
gallit0 <mateogesede@gmail.com>
2022-11-09 15:55:04 -0800

    task 3 attempt at checker

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
+    c = db.cursor()
     s = 'SELECT * FROM states WHERE name LIKE "{}"'.format(sys.argv[4])
     c.execute(s)
     rows = c.fetchall()
