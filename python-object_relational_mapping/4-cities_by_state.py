5486bf2e20a72eb36735295b05061c2534685ffd
gallit0 <mateogesede@gmail.com>
2022-11-10 04:48:06 -0800

    task 4 done

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""list all states"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(host="localhost",
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
+    except Exception:
+        print('Error')
+        exit()
+
+    c = db.cursor()
+    c.execute("""SELECT cities.id, cities.name, states.name \
+              FROM cities \
+              JOIN states WHERE cities.state_id = states.id \
+              ORDER BY cities.id""")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
