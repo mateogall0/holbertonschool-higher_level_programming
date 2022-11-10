1438ee55a731ec0ae88bdd5e556aa0f35afa8b8f
gallit0 <mateogesede@gmail.com>
2022-11-09 16:19:13 -0800

    task 3 attempt at checker

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,11 +7,15 @@ import MySQLdb
 if __name__ == "__main__":
     if len(sys.argv) < 4:
         exit()
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
+    try:
+        db = MySQLdb.connect(host="localhost",
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
+    except:
+        exit()
+
     c = db.cursor()
     s = 'SELECT * FROM states WHERE name LIKE "{}";'.format(sys.argv[4])
     c.execute(s)
