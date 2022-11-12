6ebf38795ffe48d53dfe0f86f29f943d9ef07106
gallit0 <mateogesede@gmail.com>
2022-11-12 14:28:31 -0800

    task 11 done

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""list all states"""
+
+from sys import argv
+import MySQLdb
+import sqlalchemy
+from model_state import Base, State
+from sqlalchemy.sql import select
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    try:
+        engine = create_engine(
+           'mysql+mysqldb://{}:{}@localhost:3306/{}'
+           .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+        )
+    except Exception as e:
+        print(e)
+        exit()
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    new = State(name="Louisiana")
+    s.add(new)
+    s.commit()
+    print(new.id)
+    s.close()
