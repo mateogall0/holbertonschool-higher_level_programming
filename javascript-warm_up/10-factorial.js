764968ecee9d4e2bbd5ff2637b60376ce510f0d3
gallit0 <mateogesede@gmail.com>
2022-12-06 23:53:13 +0000

    task 10 done

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const argv = require('process').argv
+const argv = require('process').argv;
 
-console.log(factorial(+argv[2]))
+console.log(factorial(+argv[2]));
 
-function factorial(n) {
-  if (isNaN(n) || n == 1) {
+function factorial (n) {
+  if (isNaN(n) || n === 1) {
     return (1);
   }
   if (n < 0) {
