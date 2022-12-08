03bf5d0015aa5e45d98306a58a893067a0c73ab7
gallit0 <mateogesede@gmail.com>
2022-12-07 17:35:58 -0800

    task 11 done

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,11 +4,5 @@ const argv = require('process').argv;
 if (!argv[2] || !argv[3]) {
   console.log(0);
 } else {
-  let tmp = +argv[2];
-  for (let i = 2; i < argv.length; i++) {
-    if (tmp < +argv[i]) {
-      tmp = +argv[i];
-    }
-  }
-  console.log(tmp);
+  console.log(argv.slice(2).sort().reverse()[1]);
 }
