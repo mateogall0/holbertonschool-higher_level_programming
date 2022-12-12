c4a825127c4abe5bccbc99959fb1889f8a950344
gallit0 <mateogesede@gmail.com>
2022-12-12 02:46:18 -0300

    task 0 done

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 const argv = require('process').argv;
 
-if (argv[2]) {
-  const f = require('fs');
-  f.readFile(argv[2], 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data);
-    }
-  });
-}
+const f = require('fs');
+f.readFile(argv[2], 'utf8', function (err, data) {
+  console.log(data || err);
+});
