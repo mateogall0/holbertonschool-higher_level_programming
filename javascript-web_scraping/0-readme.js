83d87ec3253c22e9a1584649c3436d5a2c53f115
gallit0 <mateogesede@gmail.com>
2022-12-12 02:51:04 -0300

    task 0 new attempt at checker

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const argv = require('process').argv;
-
 const f = require('fs');
-f.readFile(argv[2], 'utf8', function (err, data) {
+
+f.readFile(process.argv[2], 'utf8', function (err, data) {
   console.log(data || err);
 });
