4c08fc0fdc9151c4cc69e65ccab0cd182973f489
gallit0 <mateogesede@gmail.com>
2022-12-12 03:03:44 -0300

    task 1 fixed

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const f = require('fs');
 
-f.writeFile(process.argv[2], process.argv[3], function (err, data) {
+f.writeFile(process.argv[2], process.argv[3] || '', function (err, data) {
   if (err) console.log(err);
 });
